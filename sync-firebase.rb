df9feaa48b4937c84f9cb462e589856307e9e3f4
Jittat Fakcharoenphol <jittat@gmail.com>
2019-09-01 03:40:54 +0700

    saves submissions

--- sync-firebase.rb
+++ sync-firebase.rb
@@ -6,12 +6,41 @@ require 'json'
 BOT_USER = 'test1'
 URL = 'https://asia-east2-grader-ef0b5.cloudfunctions.net/getOldestSubmissionsInQueue'
 LIMIT = 10
+SLEEP_TIME = 60
 
-def make_submission(res)
-  puts res
+LANGUAGE_NAMES = {
+  'c_cpp' => 'cpp',
+  'python' => 'python'
+}
+
+# judge/rails initialization
+GRADER_ENV = 'grading'
+require File.join(File.dirname(__FILE__),'config/environment')
+RAILS_ENV = 'development'
+require RAILS_ROOT + '/config/environment'
+
+def create_submission(res, user)
+  problem_name = res['problem_id']
+  problem = Problem.find_by name: problem_name
+  language = Language.find_by name: LANGUAGE_NAMES[res['language']]
+
+  submission = Submission.new
+  submission.language = language
+  submission.problem = problem
+  submission.user = user
+  submission.source = res['code']
+  submission.source.encode!('UTF-8', 'UTF-8', invalid: :replace, replace: '')
+
+  submission.submitted_at = Time.new.gmtime
+  submission.save!
+
+  Task.create(submission: submission,
+              status: Task::STATUS_INQUEUE)
 end
 
 def main
+  user = User.find_by login: BOT_USER
+    
   response = Faraday.post(URL,
                           "{\"data\":{\"limit\":#{LIMIT}}}",
                           {'Content-Type' => 'application/json'})
@@ -20,7 +49,7 @@ def main
   else
     results = JSON.parse(response.body)['result']
     results.each do |res|
-      make_submission(res)
+      create_submission(res, user)
     end
   end
 end
